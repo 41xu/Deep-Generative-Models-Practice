d06ac2a5f1475b743cda822fe9ae2b10da4afd83
41xu <xu_sy11111@mail.dlut.edu.cn>
2021-04-21 10:43:55 +0800

    add

--- GAN/model.py
+++ GAN/model.py
@@ -5,12 +5,33 @@ import torch.nn.functional as F
 
 
 class Generator(nn.Module):
-    def __init__(self, shape, input_dim=64, output_size=32, output_channel=3):
+    def __init__(self, input_dim=100, output_size=32, output_channel=3):
         """
-        :param shape: b, h, c, w
-        :param input_dim: generator filters dim
+        :param input_dim: input random noise z's dim, default=100
         :param output_size:
         """
         super(Generator, self).__init__()
-        image_size = output_size
+
+        def block(in_dim, out_dim, normalize=True):
+            layers = [nn.Linear(in_dim, out_dim)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_dim, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(input_dim, 128),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, )
+        )
+
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
         
--- GAN/train.py
+++ GAN/train.py
@@ -10,7 +10,7 @@ from torchvision import datasets, transforms
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--n_epoch', type=int, default=50)
+    parser.add_argument('--n_epoch', type=int, default=1)
     parser.add_argument('--lr', type=float, default=2e-4)
     parser.add_argument('--beta', type=float, default=0.5)
     parser.add_argument('--z_dim', type=int, default=100, help="dimensionality of the latent space")
@@ -21,18 +21,21 @@ def main():
     opts = parser.parse_args()
     print(opts)
 
+    # data loader
     transform = transforms.Compose([
         transforms.Resize(opts.input_size),
         transforms.ToTensor(),
         transforms.Normalize([0.5], [0.5])
     ])
-
-    dataset = datasets.MNIST('../data/mnist', train=True, download=True, transform=transform)
+    dataset = datasets.MNIST('../data/mnist', train=True, download=False, transform=transform)
     dataloader = DataLoader(dataset, batch_size=opts.batch_size, shuffle=True)
+
+    # optimizer
+
 
     for epoch in range(opts.n_epoch):
         for i, (img, _) in enumerate(dataloader):
-            pass
+            print(img.shape) # batch_size, channel, height, width
 
 
 
